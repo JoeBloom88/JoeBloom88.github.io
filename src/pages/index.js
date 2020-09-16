ec77eddf27657675db8f94df677058dc53456023
bacon274 <jacob.fintan.mitchell.274@gmail.com>
2020-09-16 16:30:54 +0100

    index (home) page with embedded video

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,49 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import style from "./index.module.css"
 
-const IndexPage = () => (
+const IndexPage = ({data}) => {
+  const homeContent = data.homePageQuery.edges[0].node;
+  return(
+  <>
   <Layout>
     <SEO title="Home" />
+    <video height="100%" src={"https:"+ homeContent.film.file.url} loop autoPlay muted playsInline> </video>
+
+    <div className={style.info}>
+      <h1> {homeContent.title}   </h1>
+      <h2> {homeContent.description} </h2>
+      <h2> {homeContent.date} </h2>
+
+
+    </div>
    
   </Layout>
-)
+  </>
+  )
+}
 
 export default IndexPage
+
+
+export const query = graphql`
+  query {
+    homePageQuery: allContentfulHomePage {
+      edges {
+        node {
+          film {
+            file {
+              url
+            }
+          }
+          date
+          description
+          title
+        }
+      }
+    }
+  }
+  
+`
+
